73ef77fca45b50ea54323d04c9bb4f741e110242
Elena Kiryakova <errromt@mail.ru>
2021-08-06 18:42:34 +0300

    add game_functions.py refactoring alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,7 +1,7 @@
-import sys
 import pygame
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 
 def run_game():
@@ -21,9 +21,7 @@ def run_game():
     # Запуск основного цикла игры
     while True:
         # Отслеживание мобытий клавиатуры и мыши
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        gf.check_events()
 
         # При каждом проходе цикла прорисовывется экран
         screen.fill(ai_settings.bg_color)
