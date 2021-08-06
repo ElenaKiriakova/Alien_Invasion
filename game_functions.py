d559c8464cefe78a24c1eb2519ad83644fc04861
Elena Kiryakova <errromt@mail.ru>
2021-08-06 19:40:52 +0300

    refactoring game_functions.py

--- game_functions.py
+++ game_functions.py
@@ -16,19 +16,30 @@ def update_screen(ai_settings, screen, ship):
     # Отображение последнего прорисованного экрана
     pygame.display.flip()
 
+
+def check_keydown_events(event, ship):
+    """Реагирует на нажатие клавиш"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+def check_keyup_events(event, ship):
+    """Реагирует на отпускание клавиш"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
+
 def check_events(ship):
     """Обрабатывает нажатия клавиш и собития мыши"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
+            check_keydown_events(event, ship)
 
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
+            check_keyup_events(event,ship)
